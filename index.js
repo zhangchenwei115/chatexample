11bdd6e84e70d64ac9f1b2d22f57b59e913b1dbe
zhangchenwei <zhangchenwei115@163.com>
2022-07-01 00:26:17 +0200

    delete log

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-io.on('connection', (socket) => {
-    console.log(new Date(),'a user connected');
-    socket.on('disconnect', () => {
-      console.log(new Date(),'user disconnected');
-    });
-  });
 
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-    console.log(new Date(),'message: ' + msg);
+  socket.on('chat message', (msg) => {
+  console.log(new Date(),'message: ' + msg);
 });
 });
-io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
-io.on('connection', (socket) => {
-    socket.broadcast.emit('hi');
-  });
 
-  io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
   });
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
